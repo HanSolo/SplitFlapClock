23211aeb5947db5ade8d55310692f958bccaf035
Gerrit Grunwald <han.solo@mac.com>
2026-02-18 16:10:17 +0100

    Initial Commit

---
+++ SplitFlapClock/SplitFlapClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitFlapClockApp.swift
+//  SplitFlapClock
+//
+//  Created by Gerrit Grunwald on 18.02.26.
+//
+
+import SwiftUI
+
+@main
+struct SplitFlapClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
