a2db0d67e4e1a1221ea34cfd8ba761243b540255
Gerrit Grunwald <han.solo@mac.com>
2026-02-18 16:35:04 +0100

    initial code commit

--- SplitFlapClock/ClockFaceView.swift
+++ SplitFlapClock/ClockFaceView.swift
@@ -9,10 +9,17 @@ import SwiftUI
 
 struct ClockFaceView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // Clock Face
+        Circle()
+            .foregroundColor(.cyan).opacity(0.8)
+            .frame(width: 200, height: 200, alignment: .center)
+        // Ticks
+        ForEach(0..<60) {
+            Circle()
+                .foregroundColor($0.isMultiple(of: 5) ? .blue : .white)
+                .frame(width: $0.isMultiple(of: 5) ? 8 : 3, height: 8)
+                .offset(y: -90)
+                .rotationEffect(.degrees(Double($0) * 6)) // rotate the tick marks
+        }
     }
-}
-
-#Preview {
-    ClockFaceView()
 }
--- SplitFlapClock/ContentView.swift
+++ SplitFlapClock/ContentView.swift
@@ -8,17 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    //@State private var rotate = false
+    
+    
     var body: some View {
+        ZStack {
+            RotateClockView()
+        }
+        /*
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .font(.largeTitle)
+                .foregroundStyle(.yellow)
+                .rotation3DEffect(.degrees(45), axis: (x: 1, y: 0, z: 0))
         }
         .padding()
+        */
     }
-}
-
-#Preview {
-    ContentView()
 }
