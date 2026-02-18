295951bf25ee2f1314c7397802b4aa32f8208969
Gerrit Grunwald <han.solo@mac.com>
2026-02-18 16:33:29 +0100

    first commit

---
+++ SplitFlapClock/ClockFaceView.swift
@@ -0,0 +1,18 @@
+//
+//  ClockFaceView.swift
+//  SplitFlapClock
+//
+//  Created by Gerrit Grunwald on 18.02.26.
+//
+
+import SwiftUI
+
+struct ClockFaceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClockFaceView()
+}
---
+++ SplitFlapClock/RotateClockView.swift
@@ -0,0 +1,18 @@
+//
+//  RotateClockView.swift
+//  SplitFlapClock
+//
+//  Created by Gerrit Grunwald on 18.02.26.
+//
+
+import SwiftUI
+
+struct RotateClockView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RotateClockView()
+}
